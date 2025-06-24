235ba8d722db5a8b392b6446bec1ea1a9a190ff4
Hariom Modi <hariommodi7898@gmail.com>
2025-06-24 20:54:58 +0530

    Add files via upload

---
+++ public/javaScripts/boiler.js
@@ -0,0 +1,7 @@
+window.addEventListener("resize",function(e){
+    if(e.target.innerHeight<e.target.innerWidth){
+        document.querySelector("body").style.cssText = "display:none";
+    }else{
+        document.querySelector("body").style.cssText = "display:block";
+    }
+})
