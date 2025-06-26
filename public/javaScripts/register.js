333cd5bdb5f7e47027546fe3504986e7a21301d5
Hariom Modi <hariommodi7898@gmail.com>
2025-06-26 23:25:13 +0530

    Add files via upload

---
+++ public/javaScripts/register.js
@@ -0,0 +1,22 @@
+let input = document.getElementsByTagName("input");
+let label = document.getElementsByTagName("label");
+let payAndJoin = document.querySelector(".payAndJoin");
+let form = document.querySelector("form");
+
+let count;
+for(let i=0;i<input.length;i++){
+    if(input[i].value != ""){
+        label[i].classList = "clickLabel";
+    }
+    input[i].addEventListener("click",function(){
+        if(count || count==0){
+             if(input[count].value == ""){
+                label[count].classList.remove("clickLabel");
+            }
+        }
+        label[i].classList = "clickLabel";
+        // input[i].classList = "clickInput";
+        count = i;
+    })
+}
+
