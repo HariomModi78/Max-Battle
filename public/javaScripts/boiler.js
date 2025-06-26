af9d2f8f69eeed3728db90739b07ae44b82cdc3e
Hariom Modi <hariommodi7898@gmail.com>
2025-06-26 23:24:27 +0530

    Delete public directory

--- public/javaScripts/boiler.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize",function(e){
-    if(e.target.innerHeight<e.target.innerWidth){
-        document.querySelector("body").style.cssText = "display:none";
-    }else{
-        document.querySelector("body").style.cssText = "display:block";
-    }
-})
--- public/javaScripts/profile.js
+++
@@ -1,7 +0,0 @@
-let option = document.getElementsByClassName("option");
-
-for(let i=0;i<option.length;i++){
-    option[i].addEventListener("click",function(){
-        window.location.href = `/${option[i].id}`;
-    })
-}
