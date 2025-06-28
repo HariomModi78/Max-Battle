7b697d9860d3e12efa43cfe89c79367321352a42
Hariom Modi <hariommodi7898@gmail.com>
2025-06-28 14:26:36 +0530

    Delete public directory

--- public/javaScripts/footer.js
+++
@@ -1,8 +0,0 @@
-let footerBox = document.getElementsByClassName("footerBox");
-
-for(let i=0;i<footerBox.length;i++){
-    footerBox[i].addEventListener("click",function(e){
-        window.location.href = `/${e.target.id}`
-    })
-}
-
