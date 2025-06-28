8850dbf57a76e661d55e960b408a41c71cdcb1bd
Hariom Modi <hariommodi7898@gmail.com>
2025-06-29 00:16:45 +0530

    Delete public directory

--- public/public/javaScripts/header.js
+++
@@ -1,4 +0,0 @@
-let walletAmount = document.querySelector(".walletAmount");
-walletAmount.addEventListener("click",function(){
-    window.location.href = `/${walletAmount.id}`;
-})
--- public/public/script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch((err) => console.error('SW Error:', err));
-}
