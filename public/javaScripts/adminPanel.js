476890c4fc430d548582e417d8ca7135b75bb10b
Hariom Modi <hariommodi7898@gmail.com>
2025-07-03 09:53:15 +0530

    Add files via upload

---
+++ public/javaScripts/adminPanel.js
@@ -0,0 +1,31 @@
+let mobileMenu = document.querySelector(".mobileMenu");
+let sideBar = document.querySelector(".sideBar");
+
+let flag = false;
+mobileMenu.addEventListener("click",function(){
+    if(flag==false){
+        sideBar.classList = "mobileSideBar";
+        sideBar.style.cssText = "display:flex";
+        flag = true;
+    }else{
+        console.log("Working");
+        sideBar.style.cssText = "display:none";
+        flag = false;
+    }
+})
+
+let events = document.getElementsByClassName("event");
+let option = document.getElementsByClassName("option");
+
+for(let i=0;i<events.length;i++){
+    events[i].addEventListener("click",function(){
+        window.location.href = `/${events[i].id}`;
+    })
+}
+
+for(let i=0;i<option.length;i++){
+    option[i].addEventListener("click",function(){
+        window.location.href = `/${option[i].id}`;
+    })
+}
+
