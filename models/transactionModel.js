273cca0fcf06aaf6638a93657f4ed04c13186210
Hariom Modi <hariommodi7898@gmail.com>
2025-07-27 13:49:54 +0530

    Add files via upload

---
+++ models/transactionModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  paymentId: { type: String,unique:true},     // Razorpay Payment ID
+  orderId: { type: String},                      // Razorpay Order ID
+  amount: { type: Number},                       // In paise (e.g., 50000 = ₹500)
+  currency: { type: String, default: 'INR' },
+  status: { type: String},                       // captured, failed, etc.
+  createdAt: { type: Date, default: Date.now },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },  // Optional: link to user
+  upiId:String,
+  flag:{
+    type:Boolean,
+    default:false
+  }
+})
+
+module.exports = mongoose.model('transaction', transactionSchema);
